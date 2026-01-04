d6a07980e776c398b12749e21228330a282d36b4
Edward Wawrzynski <edward.wawrzynski@protonmail.com>
2026-01-04 14:46:40 -0500

    add aliases for proton w/ underscores for compatibility with previous installations

--- flake.nix
+++ flake.nix
@@ -70,6 +70,11 @@
           };
         };
 
+      proton = mkProton "proton-cachyos" "base";
+      protonv2 = mkProton "proton-cachyos-x86_64-v2" "x86_64-v2";
+      protonv3 = mkProton "proton-cachyos-x86_64-v3" "x86_64-v3";
+      protonv4 = mkProton "proton-cachyos-x86_64-v4" "x86_64-v4";
+
     in
     {
       packages.${system} = {
@@ -81,10 +86,15 @@
         wayland-protocols-git = mesaPkgs.wayland-protocols-git;
 
         # Proton CachyOS
-        proton-cachyos = mkProton "proton-cachyos" "base";
-        proton-cachyos-x86_64-v2 = mkProton "proton-cachyos-x86_64-v2" "x86_64-v2";
-        proton-cachyos-x86_64-v3 = mkProton "proton-cachyos-x86_64-v3" "x86_64-v3";
-        proton-cachyos-x86_64-v4 = mkProton "proton-cachyos-x86_64-v4" "x86_64-v4";
+        proton-cachyos = proton;
+        proton-cachyos-x86_64-v2 = protonv2;
+        proton-cachyos-x86_64-v3 = protonv3;
+        proton-cachyos-x86_64-v4 = protonv4;
+
+        # Deprecated aliases (w/ underscores) for backwards compatibility
+        proton-cachyos-x86_64_v2 = protonv2;
+        proton-cachyos-x86_64_v3 = protonv3;
+        proton-cachyos-x86_64_v4 = protonv4;
 
         # Vintage Story
         vintagestory = mkVintageStory "vintagestory";
@@ -136,6 +146,11 @@
           proton-cachyos-x86_64-v2 = self.packages.${final.system}.proton-cachyos-x86_64-v2;
           proton-cachyos-x86_64-v3 = self.packages.${final.system}.proton-cachyos-x86_64-v3;
           proton-cachyos-x86_64-v4 = self.packages.${final.system}.proton-cachyos-x86_64-v4;
+
+          # Deprecated aliases w/ underscores
+          proton-cachyos-x86_64_v2 = self.packages.${final.system}.proton-cachyos-x86_64-v2;
+          proton-cachyos-x86_64_v3 = self.packages.${final.system}.proton-cachyos-x86_64-v3;
+          proton-cachyos-x86_64_v4 = self.packages.${final.system}.proton-cachyos-x86_64-v4;
         };
 
         vintagestory = final: prev: {
