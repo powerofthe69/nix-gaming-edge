d825bfdb0e69f6972f1fe85d2891e6ea3e4f89c0
Edward Wawrzynski <edward.wawrzynski@protonmail.com>
2026-04-21 23:32:55 -0400

    really fix harkinian override

--- pkgs/harkinian/default.nix
+++ pkgs/harkinian/default.nix
@@ -14,14 +14,31 @@ let
       echo "${version}" > $out/GIT_COMMIT_TAG
       echo "${builtins.substring 0 7 src.rev}" > $out/GIT_COMMIT_HASH
     '';
+
+  shipwrightSrcFixed = createMetadata "shipwright" shipwrightSrc.src shipwrightSrc.version;
+  _2ship2harkinianSrcFixed =
+    createMetadata "2ship2harkinian" _2ship2harkinianSrc.src
+      _2ship2harkinianSrc.version;
 in
 {
-  shipwright = pkgs.shipwright.overrideAttrs {
-    version = shipwrightSrc.version;
-    src = createMetadata "shipwright" shipwrightSrc.src shipwrightSrc.version;
-  };
-  _2ship2harkinian = pkgs._2ship2harkinian.overrideAttrs {
-    version = _2ship2harkinianSrc.version;
-    src = createMetadata "2ship2harkinian" _2ship2harkinianSrc.src _2ship2harkinianSrc.version;
-  };
+  shipwright =
+    (pkgs.shipwright.override {
+      shipwright = {
+        src = shipwrightSrcFixed;
+      };
+    }).overrideAttrs
+      {
+        version = shipwrightSrc.version;
+        src = shipwrightSrcFixed;
+      };
+  _2ship2harkinian =
+    (pkgs._2ship2harkinian.override {
+      _2ship2harkinian = {
+        src = _2ship2harkinianSrcFixed;
+      };
+    }).overrideAttrs
+      {
+        version = _2ship2harkinianSrc.version;
+        src = _2ship2harkinianSrcFixed;
+      };
 }
